ca4d5df84de3bae08b4a72474a7a957bdbf5a28b
GabrielTrossero <gabrieltrosserogetr@gmail.com>
2021-08-10 14:46:30 -0300

    definicion de las migraciones

---
+++ database/migrations/2021_08_09_203113_posts.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Posts extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('titulo', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('description')->nullable();
+            $table->dateTime('created', $precision = 0);
+            $table->dateTime('modified', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('post');
+    }
+}
